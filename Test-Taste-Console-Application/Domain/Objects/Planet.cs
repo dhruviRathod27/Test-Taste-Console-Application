7d50db829bd407017fa9e2450f41698c008c1ea7
Dhruvita Rathod <dhruvita.rathod27@gmail.com>
2022-09-10 13:49:55 +0530

    Add project files.

---
+++ Test-Taste-Console-Application/Domain/Objects/Planet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Planet
+    {
+        public string Id { get; set; }
+        public float SemiMajorAxis { get; set; }
+        public ICollection<Moon> Moons { get; set; }
+        public double AverageMoonGravity
+        {
+            get; set;
+        } 
+
+        public Planet(PlanetDto planetDto)
+        {
+            Id = planetDto.Id;
+            SemiMajorAxis = planetDto.SemiMajorAxis;
+            Moons = new Collection<Moon>();
+            if(planetDto.Moons != null)
+            {
+                double sum=0.0;
+                var len = planetDto.Moons.Count;
+                foreach (MoonDto moonDto in planetDto.Moons)
+                {
+                    Moons.Add(new Moon(moonDto));
+                    sum=sum+ moonDto.Gravity;
+                }
+                AverageMoonGravity = sum / len;
+            }
+        }
+
+        public Boolean HasMoons()
+        {
+            return (Moons != null && Moons.Count > 0);
+        }
+    }
+}
